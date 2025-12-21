565a5ff3d2605a9989fe563a70ed1c465898cb46
proto-fmt <159235179+proto-fmt@users.noreply.github.com>
2025-12-21 12:41:21 +0500

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -43,7 +43,7 @@ pacman -Syu --noconfirm\
   
    #another 
    perl-image-exiftool mat2 \ # remove meta photo and files
-   traceroute inetutils nmap openbsd-netcat whois \
+   traceroute inetutils nmap openbsd-netcat whois bind \
 
 # от пользователя sudo
 sudo systemctl --user enable --now pipewire wireplumber
